1e32e44405be6e4f11e945bd6a384bbcd6ceebdc
Rishi Noule <141018161+Rishi1403348@users.noreply.github.com>
2024-07-25 10:06:32 +0530

    Add files via upload

--- MyFirstDjango/calc/views.py
+++ MyFirstDjango/calc/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 def home(request):
     return render(request,'home.html',{'name':'Raju'})
+
+def add(request):
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    val3=val1+val2
+    return render(request,'results.html',{'result':val3})
